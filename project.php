ca441554c6840af10524d1340d314ce54ed901dd
Nathan Ryder <nathanryder16@gmail.com>
2018-05-25 12:38:33 +0100

    Finished project description page

--- project.php
+++ project.php
@@ -10,6 +10,10 @@
     <?php include 'files/include/navbar.php'; ?>
 
     <div class="container white-bg-panel">
+
+      <?php
+      if ($_GET['project'] === "test") {
+      ?>
       <div class="row">
         <div class="col-lg-3">
           <h4>NAME OF PROJECT</h4>
@@ -37,13 +41,71 @@
       <div class="row">
         <div class="col-lg-3">
         </div>
-        <div class="col-lg-9" style="text-align: justify !important; padding-right: 60px; font-size: 19px;">
+        <div class="col-lg-9 projectContent">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
         </div>
       </div>
+    <?php
+      } else {
+
+        $query = mysqli_query($con, "SELECT * FROM projects WHERE ID=" . $_GET['project']);
+        $data = mysqli_fetch_assoc($query);
+
+        if ($data == NULL) {
+          header("Location: index.php");
+          die();
+        }
+
+        $catQuery = mysqli_query($con, "SELECT Name FROM categories WHERE ID=" . $data['Category']);
+        $cat = mysqli_fetch_assoc($catQuery)["Name"];
+
+        $header = "uploads/" . $data["Folder"] . "/" . $data["Header"];
+        $content = file_get_contents("uploads/" . $data["Folder"] . "/content.html");
+    ?>
+    <div class="row">
+      <div class="col-lg-3">
+        <h4><?php echo strToUpper($data["Name"]) ?></h4>
+        <br><br><br>
+
+        <h5>Year</h5>
+        <h3><?php echo $data["Year"] ?></h3>
+        <br><br><br>
+
+        <h5>Downloads</h5>
+        <?php
+        if ($data["File"] !== NULL) {
+          echo '<h3><a class="download-links" href="#">Download</a></h3>';
+        }
+        if ($data["Repository"] !== "") {
+          echo '<h3><a class="download-links" href="#">Github</a></h3>';
+        }
+        ?>
+        <br><br><br>
+
+        <h5>Category</h5>
+        <h3><?php echo $cat ?></h3>
+      </div>
+
+      <div class="col-lg-8">
+        <img style="width:100%;" src="<?php echo $header ?>">
+      </div>
+    </div>
+    <br><br>
+
+    <div class="row">
+      <div class="col-lg-3">
+      </div>
+      <div class="col-lg-9 projectContent">
+        <?php echo $content; ?>
+      </div>
+    </div>
+    <?php
+      }
+    ?>
+
     </div>
 
 
